02bcd62d5224d7ad30fe3ed77f87c77d02ca8e32
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-02-26 18:26:52 +0100

    Trim

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,17 @@ import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
-import MainPage from './components/MainPage'
-import { Provider } from 'react-redux'
-import store from './redux/store'
+// import { Provider } from 'react-redux'
+// import store from './redux/store'
 function App() {
 
 
   return (
     <>
-    <Provider store={store}>
     <BrowserRouter>
 <NavBar/>
-<MainPage/>
  <Footer/>
     </BrowserRouter>
-    </Provider>
     </>
   )
 }
